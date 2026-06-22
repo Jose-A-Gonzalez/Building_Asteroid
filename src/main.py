c99fcc3a13687b22a78502af24e7aaa5d02a435e
Jose-A-Gonzalez <jose.gonzalez148@upr.edu>
2026-06-22 02:01:08 -0400

    C: added player drawing and triangle rendering

--- src/main.py
+++ src/main.py
@@ -1,21 +1,24 @@
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
 import pygame
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     clock = pygame.time.Clock()
     dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    screen.fill("black")
-    pygame.display.flip()
+    
     while True:
         log_state()
         for event in pygame.event.get():
            if event.type == pygame.QUIT:
               return
         dt = clock.tick(60)/1000
-
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
 if __name__ == "__main__":
     main()
