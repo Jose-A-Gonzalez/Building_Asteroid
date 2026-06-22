789c41c80c66c78f59b52cc5eb426346b5dd0cf7
Jose-A-Gonzalez <jose.gonzalez148@upr.edu>
2026-06-22 02:11:12 -0400

    D: added rotation logic

--- src/player.py
+++ src/player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
 import pygame
 class Player(CircleShape):
     def __init__(self, x: float, y:float):
@@ -17,4 +17,14 @@ class Player(CircleShape):
     
     def draw(self,screen : "Surface"):
         pygame.draw.polygon(screen,"white",self.triangle(),LINE_WIDTH)
+    
+    def rotate(self, dt : float):
+        self.rotation += PLAYER_TURN_SPEED * dt
 
+    def update(self, dt: float) -> None:
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
